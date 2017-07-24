fbb45a74025afebe37411ef1a951574ee1e5cde3
Renata Mendonca <renata.mendonca@localiza.com>
2017-07-24 09:03:10 -0300

    alteracao interface

---
+++ MerchantGuide.Model/INumeralModel.cs
@@ -0,0 +1,12 @@
+﻿namespace MerchantGuide.Model
+{
+    interface INumeralModel
+    {
+        string Text { get; set; }
+        int AbsoluteValue { get; set; }
+
+        int CalculateAbsoluteValue();
+
+        bool NumberIsValid(string text);
+    }
+}
---
+++ MerchantGuide.Model/NumeralModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MerchantGuide.Model
+{
+    public abstract class NumeralModel : INumeralModel
+    {
+        public string Text { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int AbsoluteValue { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public NumeralModel(string text)
+        {
+            if (NumberIsValid(text))
+            {
+                Text = text;
+                AbsoluteValue = CalculateAbsoluteValue();
+            }
+            else
+            {
+                throw new ArgumentException("Invalid value for: Text");
+            }
+        }
+
+        public int CalculateAbsoluteValue()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool NumberIsValid(string text)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
